e0847f39d2dc9bee64bef184f80a5ac3422d79c2
zhouwen <linker784533@126.com>
2017-09-26 14:52:56 +0800

    修改层级关系
    Change-Id: I9fae4ceacad8e39b77fc50e707a1f2562acbf688

---
+++ ZWHybird/TestPlugin.h
@@ -0,0 +1,13 @@
+//
+//  TestPlugin.h
+//  UPHybirdDemo
+//
+//  Created by Steven Zhou on 2017/9/21.
+//  Copyright © 2017年 Steven Zhou. All rights reserved.
+//
+
+#import <Cordova/CDVPlugin.h>
+
+@interface TestPlugin : CDVPlugin
++(void)testCordova;
+@end
